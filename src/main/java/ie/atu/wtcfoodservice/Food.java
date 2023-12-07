1f36b9546bb748c394c715f81b9f3bfe297e88e1
Denis Costello <G00396309@atu.ie>
2023-12-07 20:34:43 +0000

    Added validation to Food class

--- src/main/java/ie/atu/wtcfoodservice/Food.java
+++ src/main/java/ie/atu/wtcfoodservice/Food.java
@@ -1,9 +1,17 @@
 package ie.atu.wtcfoodservice;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class Food {
+    @NotBlank(message = "FoodName cannot not blank")
+    private String foodName;
+
+    @NotBlank(message = "Food Image cannot be blank")
+    private String foodImage;
+
+
 }
