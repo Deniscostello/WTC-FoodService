a2b861cf662eda49ff092c8b044ca4a0165016bd
Denis Costello <G00396309@atu.ie>
2023-12-07 20:31:18 +0000

    Added getmapping to get all foods

--- src/main/java/ie/atu/wtcfoodservice/FoodController.java
+++ src/main/java/ie/atu/wtcfoodservice/FoodController.java
@@ -1,7 +1,14 @@
 package ie.atu.wtcfoodservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FoodController {
+
+    @GetMapping("food/getAllfood")
+    public Food getAllFood(@RequestBody Food food){
+        return food;
+    }
 }
