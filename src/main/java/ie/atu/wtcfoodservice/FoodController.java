18ddc0a2bb23b61ceff6be38404da1b2699ec2b4
Denis Costello <G00396309@atu.ie>
2023-12-07 20:50:45 +0000

    Change get mapping to response entity

--- src/main/java/ie/atu/wtcfoodservice/FoodController.java
+++ src/main/java/ie/atu/wtcfoodservice/FoodController.java
@@ -1,9 +1,7 @@
 package ie.atu.wtcfoodservice;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
