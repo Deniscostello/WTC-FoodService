14ede5f111a211c5d155fd161accee069540ad37
Denis Costello <G00396309@atu.ie>
2023-12-09 22:05:40 +0000

    Added entity and id to food class

--- src/main/java/ie/atu/wtcfoodservice/Food.java
+++ src/main/java/ie/atu/wtcfoodservice/Food.java
@@ -1,14 +1,20 @@
 package ie.atu.wtcfoodservice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Food {
+    @Id
+    private Long id;
+
     @NotBlank(message = "FoodName cannot not blank")
     private String foodName;
 
