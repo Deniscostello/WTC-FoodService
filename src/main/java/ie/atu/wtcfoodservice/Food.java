23b5451ccb1dfd3bef8f55cfaaae453cc29eecfe
Deniscostello <114572537+Deniscostello@users.noreply.github.com>
2023-12-10 13:22:00 +0000

    Revert "Mongodb branch"

--- src/main/java/ie/atu/wtcfoodservice/Food.java
+++ src/main/java/ie/atu/wtcfoodservice/Food.java
@@ -1,30 +1,19 @@
 package ie.atu.wtcfoodservice;
 
-
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.annotation.Id;
 
 @Data
-@Document(collection = "foods")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Food {
-    @Id
-    private ObjectId id;
-
-    @NotBlank(message = "Food Name cannot not blank")
+    @NotBlank(message = "FoodName cannot not blank")
     private String foodName;
 
     @NotBlank(message = "Food Image cannot be blank")
     private String foodImage;
-
-    @NotBlank(message = "Prediction cannot be blank")
-    private String prediction;
 
 
 }
