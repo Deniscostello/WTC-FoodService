6307e357f6efd2403e5b5fd0a2a188cc56f4b880
Denis Costello <G00396309@atu.ie>
2023-12-10 13:55:36 +0000

    Return a map of "foods" that contains a list of foods found in db

--- src/main/java/ie/atu/wtcfoodservice/FoodRepository.java
+++ src/main/java/ie/atu/wtcfoodservice/FoodRepository.java
@@ -2,6 +2,8 @@ package ie.atu.wtcfoodservice;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodRepository extends MongoRepository<Food, ObjectId> {
 }
