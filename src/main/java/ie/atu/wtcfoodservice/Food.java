6053bfb6f1473fe898f130e30517b43b556028df
Denis Costello <G00396309@atu.ie>
2023-12-10 13:46:15 +0000

    added document annotation for foods collection

--- src/main/java/ie/atu/wtcfoodservice/Food.java
+++ src/main/java/ie/atu/wtcfoodservice/Food.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
+@Document(collection = "foods")
 @AllArgsConstructor
 public class Food {
     @NotBlank(message = "FoodName cannot not blank")
